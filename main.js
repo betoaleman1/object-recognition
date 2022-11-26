f9decc11067039dad743f3d04ffe1941fe5a0f84
betoaleman1 <105089286+betoaleman1@users.noreply.github.com>
2022-11-25 19:47:36 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 img=" ";
 
+status=" ";
+
 function setup(){
     canvas=createCanvas(500,410);
     canvas.center();
+    objectDetector=ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML="status:detectando objetos...";
 }
 
 function preload(){
@@ -16,4 +20,22 @@ function draw(){
     noFill();
     stroke("FF0000");
     rect(25,55,420,380);
+    fill("#0000FF");
+    text("gato",255,85)
+    noFill();
+    stroke("0000FF");
+    rect(235,65,210,380)
+}
+
+function modelLoaded(){
+    console.log("modelo cargado");
+    status="true";
+    objectDetector.detect(img,gotResult);
+}
+
+function gotResult(error,results){
+    if(error){
+     console.log(error);   
+    }
+    console.log(results);
 }
